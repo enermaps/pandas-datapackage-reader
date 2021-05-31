2297c33d926f8b1b047e1b0c1a676599d75350b8
Giuseppe PERONATO <giuseppe.peronato@idiap.ch>
2021-05-31 11:15:54 +0200

    add custom delimiter

--- pandas_datapackage_reader/__init__.py
+++ pandas_datapackage_reader/__init__.py
@@ -29,7 +29,7 @@ __version__ = get_versions()["version"]
 del get_versions
 
 
-def read_datapackage(dp, resource_name=None, separator=","):
+def read_datapackage(dp, resource_name=None, delimiter=","):
     """
     Read tabular CSV files from Data Packages into DataFrames.
 
@@ -41,7 +41,7 @@ def read_datapackage(dp, resource_name=None, separator=","):
     resource_name: string or list of strings
         Name or names of resources to read. Lists of strings are used to
         request multiple resources.
-    separator: string
+    delimiter: string
         Separator in CSV file. Default = ",".
 
     Returns
@@ -146,7 +146,7 @@ def read_datapackage(dp, resource_name=None, separator=","):
                 thousands=thousands_sep,
                 decimal=decimal_char,
                 encoding=encoding,
-                separator=separator,
+                delimiter=delimiter,
             )
         elif format == "geojson":
             import geopandas
